568caa4434205b8378f60bc1f11977e20445f05a
Alexander Popov <iiiypuk@fastmail.fm>
2017-10-29 01:06:47 +0300

    Add image resolution to title url

--- ytcg.js
+++ ytcg.js
@@ -1,7 +1,13 @@
 function getCover()
 {
   var videoUrl = document.getElementById('videoUrl').value;
-  var coverUrl = 'https://img.youtube.com/vi/' + YouTubeGetID(videoUrl) + '/maxresdefault.jpg';
-  document.getElementById('ytImage').innerHTML =
-    '<a download href="' + coverUrl + '"><img src="'+ coverUrl +'"></a>';
+  var videoCover = new Image();
+  
+  videoCover.onload = function()
+  {
+    document.getElementById('ytImage').innerHTML =
+    '<a title="' + videoCover.width + 'x' + videoCover.height + '" download href="' + videoCover.src + '"><img src="'+ videoCover.src +'"></a>';
+  }
+
+  videoCover.src = 'https://img.youtube.com/vi/' + YouTubeGetID(videoUrl) + '/maxresdefault.jpg';
 }
