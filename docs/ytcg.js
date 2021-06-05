36f0ffb03d93146931c887d0736291c3293e5d7d
Alexander Popov <iiiypuk@iiiypuk.me>
2021-06-05 18:01:32 +0300

    download by click

--- docs/ytcg.js
+++ docs/ytcg.js
@@ -6,7 +6,7 @@ function getCover()
   videoCover.onload = function()
   {
     document.getElementById('ytImage').innerHTML =
-    '<a title="' + videoCover.width + 'x' + videoCover.height + '" download href="' + videoCover.src + '"><img src="'+ videoCover.src +'"></a>';
+    '<a title="' + videoCover.width + 'x' + videoCover.height + '" href="' + videoCover.src + '" download="cover"><img src="'+ videoCover.src +'"></a>';
   }
 
   videoCover.src = 'https://img.youtube.com/vi/' + YouTubeGetID(videoUrl) + '/maxresdefault.jpg';
